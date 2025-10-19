5fd01f11d1078784e75d6a1d48261ea12ad42da9
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-19 10:56:42 +0530

    Some practice

---
+++ tuple_exercise.py
@@ -0,0 +1,19 @@
+tup = (2,3,4,5,6,8,7,9,2,2,5,6,)
+print(tup)
+
+print(tup[0])
+print(tup[1])
+
+tup =(1.0,)
+print(tup)
+print(type(tup))
+
+
+tup = (1,2,3,4,5,6,7,8,9)
+print(tup[1:4])
+
+tup = (1,2,3,4,5,6,7,8,9)
+print(tup.index(3))
+
+tup = (1,2,3,4,5,6,7,8,9)
+print(tup.count(3))
