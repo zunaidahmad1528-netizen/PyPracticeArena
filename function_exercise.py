d7d49a6788dc121bb8aaaabbaffad07b97b3d47f
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-22 17:43:10 +0530

    More practice

---
+++ function_exercise.py
@@ -0,0 +1,69 @@
+a = 5 
+b = 10
+sum  = a + b
+print(sum)
+# more codes 
+a = 2
+b = 10
+sum = a + b
+print(sum)
+# more code
+a = 1
+b = 23
+sum = a + b
+print(sum)
+
+def cul_add(a,b):
+    add = a + b
+    print(add)
+    return add
+# more codes 
+cul_add(2,3)
+
+# more codes line 
+def culmult(a,b,c):
+    multi = a * b * c
+    print(multi)
+    return multi
+# more codes line 
+culmult(2,2,2)
+
+# more codes line 
+def culsub(a,b,c,d,f,e,g,s):
+
+    sub = a - b - c - d - f - e - g - s
+    print(sub)
+    return sub
+    
+    # more lines of codes 
+culsub(2,3,46,5,3,33,5,7)
+
+# more lines of codes 
+def culdev(a,b,c):
+    dev = a / b /c
+    print(dev)
+    return dev
+    # more lines of codes 
+culdev(2,3,1)
+def printhello():
+    print("hello")
+    return print
+
+printhello()
+printhello()
+printhello()
+printhello()
+printhello()
+printhello()
+
+def averageof3no(a,b,c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+averageof3no(1,2,3)
+
+def cul_prod(a,b):
+    print(a * b)
+    return a * b
+
+cul_prod()
