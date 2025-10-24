2233148cbd08bccacc752014e2e79db8428a87af
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-24 15:17:09 +0530

    Loops excerise

---
+++ loops_exercise.py
@@ -0,0 +1,53 @@
+i = 1
+while i<=5:
+  
+    print("hello")
+#     i += 1
+i = 5
+while i >= 1:
+#     print(i)
+#     i -= 1
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+      
+      
+i = 10 
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+
+i = 100 
+while i >= 1:
+    print(i)
+    i -= 1
+n = int(input("enter a number :"))
+i = 1 
+while i <= 10:
+    print(i*n)
+    i += 1
+
+i = 1
+while i <= 10:
+    print(i*i)
+    i += 1
+
+heros = ["spider" , "ironman", "thor", "hulk", "captain america"]
+x = "hulk"
+ind = 0 
+while ind < len(heros):
+    if (heros[ind] == x):
+        print("found hulk")
+        break
+    else:
+        print("findind...")
+    ind += 1
