3c7293af6174d6fd17132660ecc2a7dd17f9deb3
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-30 00:43:02 +0530

    Oops commit

---
+++ oops_exercise.py
@@ -0,0 +1,32 @@
+class Students:
+    name = "zunaid"
+s1 = Students()
+print(s1.name)
+
+s2 = Students()
+print(s2.name)
+
+class Car:
+    color = "blue"
+    brand = "mercedes"
+    name = "new car"
+car1 = Car()
+print(car1.color)
+print(car1.brand)
+print(car1.name)
+
+
+# more codes od line
+class Students():
+    name  = "zunaid"
+    def __init__():
+        print("adding new students in database..")
+s1 = Students()
+print(s1)
+class Students:
+    
+    def __init__(self):
+        print(self)
+        print("adding new student in database..")
+        
+s1 = Students()
