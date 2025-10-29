f65f1da30933aa44bbee888016039d62ecd0f387
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-30 00:45:28 +0530

    commits

--- oops_exercise.py
+++ oops_exercise.py
@@ -48,3 +48,42 @@ class Students:
         self.age = age
         self.sex = sex
         print("adding some or more data..")
+        
+s1 = Students("zunaid", 97 , 19 , "male")
+print(s1.name, s1.marks , s1.age , s1.sex)
+
+s2 = Students("rihan", 89 , 19 , "i dont know may be he is a gay")
+print(s2.marks , s2.age , s2.name , s2.sex)
+
+class Car:
+    name = "toyota"
+    colour = "red"
+    size = "large"
+    
+s1 = Car()
+print(s1)
+
+class Car:
+    name = "swift"
+    colour = "black"
+    size = "medium"
+    model = 2025
+s1 = Car()
+s2 = Car()
+s3 = Car()
+s4 = Car()
+s5 = Car()
+print(s1.name)
+print(s2.name)
+print(s3.name)
+print(s4.name)
+print(s5.name)
+print(s1.colour)
+print(s2.colour)
+print(s3.colour)
+print(s2.colour ,s2.colour)
+class Student:
+    name = "zunaid"
+    marks = 97
+    age = 19
+    
