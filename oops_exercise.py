222168a24a00bc9be9a76b6eba58d32e6184e9f7
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-30 00:44:20 +0530

    More excerise

--- oops_exercise.py
+++ oops_exercise.py
@@ -30,3 +30,21 @@ class Students:
         print("adding new student in database..")
         
 s1 = Students()
+
+class Students:
+    def __init__(self, fullname):
+        self.name = fullname
+        print("adding new students in database..")
+        
+s1 = Students("zunaid")
+print(s1.name)
+s2 = Students("mohd")
+print(s2.name)
+        
+class Students:
+    def __init__(self,name,marks,age,sex):
+        self.name = name
+        self.marks = marks
+        self.age = age
+        self.sex = sex
+        print("adding some or more data..")
