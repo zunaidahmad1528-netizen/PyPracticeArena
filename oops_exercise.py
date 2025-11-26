af295df804ebea4edb06d9b343baa558973a1b74
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-11-26 13:34:41 +0530

    .......

--- oops_exercise.py
+++ oops_exercise.py
@@ -454,13 +454,42 @@
 # multiple inheritance
 
 
-class A:
-    varA = "I am class A"
-class B:
-    varB = "I am class B"
-class C(A, B):
-    varC = "I am class C"
-c1 = C()
-print(c1.varA)
-print(c1.varB)
-print(c1.varC)
+# class A:
+#     varA = "I am class A"
+# class B:
+#     varB = "I am class B"
+# class C(A, B):
+#     varC = "I am class C"
+# c1 = C()
+# print(c1.varA)
+# print(c1.varB)
+# print(c1.varC)
+
+#  SUPER METHOD
+# super() method is used to access methods of the parent class.
+
+class Car:
+    def __init__ (self, type):
+        self.type = type
+    
+    @staticmethod 
+    def start():
+        print('car started...')
+     
+    @staticmethod   
+    def stop():
+        print("car stopped...")
+    
+class ToyotaCar(Car):
+    def __init__ (self, model, name, type):
+        self.name = name
+        self.model = model
+        super().__init__(type)
+        
+        
+car1 = ToyotaCar("2025", "Fourtuner", "suv")
+print(car1.name)
+print(car1.type)
+print(car1.model)
+car1.start()
+car1.stop()
