1f6fddc2aeeea14c34983ec3c023d7c92345996a
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2026-01-07 23:30:36 +0530

    some ...

--- recursion_exercise.py
+++ recursion_exercise.py
@@ -245,3 +245,22 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 print(fibonacci(6))
+
+
+def longest_unique_substring(s):
+    seen = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+
+        seen.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+
+print(longest_unique_substring("abcabcbb"))  # Output: 3
